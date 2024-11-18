88bc4cbd9ddbd6a6866e4f480341f11796a09ba1
DevPirateSA <kellymahlangu01@gmail.com>
2024-11-18 03:42:44 +0200

    yring

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  console.log(name)
-  return res.send("Hello World")
+    const { name = 'World' } = req.query
+    console.log(name)
+    return res.send("Hello World")
 }
